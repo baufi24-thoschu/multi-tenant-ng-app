ea8ca7ce41a564b01e922bbd736fa6df40ffe4f7
DESKTOP-NUTUO9P\thoma <thoschulte@googlemail.com>
2021-09-23 00:22:12 +0200

    yapp

--- libs/bar-list/src/lib/bar-list/bar-list.component.ts
+++ libs/bar-list/src/lib/bar-list/bar-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { pick, prop } from 'ramda';
 
 @Component({
   selector: 'multi-tenant-ng-app-bar-list',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-list.component.scss']
 })
 export class BarListComponent implements OnInit {
-  //constructor() { }
+  constructor(private readonly renderer: Renderer2, private readonly el: ElementRef, private readonly keycloakService: KeycloakService) {
+    console.log('*** BarListComponent ***');
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Do stuff with Renderer2 and the config from keycloak
+    const idTokenParsed = this.keycloakService.getKeycloakInstance().idTokenParsed;
+    const idTokenParsedNamePicked = pick(['name'], idTokenParsed);
+    const div = this.renderer.createElement('div');
+    const text = this.renderer.createText(`Hello world from BarListComponent by ${JSON.stringify(idTokenParsedNamePicked)}`);
+
+    this.renderer.appendChild(div, text);
+    this.renderer.appendChild(this.el.nativeElement, div);
+  }
 }
--- libs/baz-list/src/lib/baz-list/baz-list.component.ts
+++ libs/baz-list/src/lib/baz-list/baz-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { pick } from 'ramda';
 
 @Component({
   selector: 'multi-tenant-ng-app-baz-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./baz-list.component.scss']
 })
 export class BazListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly renderer: Renderer2, private readonly el: ElementRef, private readonly keycloakService: KeycloakService) {
+    console.log('*** BazListComponent ***');
   }
 
+  ngOnInit(): void {
+    // Do stuff with Renderer2 and the config from keycloak
+    const idTokenParsed = this.keycloakService.getKeycloakInstance().idTokenParsed;
+    const idTokenParsedNamePicked = pick(['name'], idTokenParsed);
+    const div = this.renderer.createElement('div');
+    const text = this.renderer.createText(`Hello world from BazListComponent by ${JSON.stringify(idTokenParsedNamePicked)}`);
+
+    this.renderer.appendChild(div, text);
+    this.renderer.appendChild(this.el.nativeElement, div);
+  }
 }
