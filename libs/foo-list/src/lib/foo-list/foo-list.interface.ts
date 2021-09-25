2165062afdea46adf5b010b7fc023664dda94728
DESKTOP-NUTUO9P\thoma <thoschulte@googlemail.com>
2021-09-26 01:22:20 +0200

    update

--- libs/foo-list/src/lib/foo-list/foo-list.interface.ts
+++ libs/foo-list/src/lib/foo-list/foo-list.interface.ts
@@ -3,5 +3,5 @@ import { KeycloakTokenParsed } from 'keycloak-js';
 export interface FooListInterface extends KeycloakTokenParsed {
   avatar_url: string;
   name: string;
-  config: Record<'foo' | 'bar' | 'baz', string>;
+  config: Record<'foo1' | 'bar1' | 'baz1', string | Record<'foo2' | 'bar2' | 'baz3', string | number | boolean>>;
 }
